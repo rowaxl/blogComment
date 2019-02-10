94e90057f88c7459ef6d93f7f0442b7a03c96b1d
rowaxl <rowaxl0@gmail.com>
2019-02-10 21:27:21 +0900

    initialize redux project

---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostList extends Component { 
+    render() {
+        return (
+            <div>
+                PostList
+            </div>
+        );
+    };
+};
+
+export default PostList;
